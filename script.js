c26a9d2ff443c4715f0b08e42d253406d91529c3
Betty Anderson <101119141+bettyand@users.noreply.github.com>
2022-04-01 17:11:10 -0400

    bug fix

--- script.js
+++ script.js
@@ -2,9 +2,6 @@ const list = document.getElementsByTagName("ul")[0];
 const textInput = document.getElementsByTagName("input")[0];
 textInput.value = "";
 const button = document.getElementsByTagName("button")[0];
-const listDefaults = document.getElementsByTagName("li");
-listDefaults.map(this.onclick = e => list.removeChild(e.target));
-
 
 button.onclick = e => {
     let newItem = document.createElement("li");
