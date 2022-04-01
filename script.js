8a8e66fb7e313dd8f3cdc42902e15e8b7ca0f28d
Betty Anderson <101119141+bettyand@users.noreply.github.com>
2022-04-01 16:51:51 -0400

    add css

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-const button = document.getElementsByTagName("button");
+const list = document.getElementsByTagName("ul")[0];
+const textInput = document.getElementsByTagName("input")[0];
+textInput.value = "";
+const button = document.getElementsByTagName("button")[0];
+const listDefaults = document.getElementsByTagName("li");
+listDefaults.map(this.onclick = e => list.removeChild(e.target));
 
-const list = document.getElementsByTagName("ul");
 
-const listItem = document.getElementsByTagName("li");
-
-button.addEventListener("click", () => {
+button.onclick = e => {
     let newItem = document.createElement("li");
-    newItem.innerHTML = document.querySelector("#input");
+    textVal = textInput.value;
+    newItem.textContent = textVal;
+    newItem.onclick = e => list.removeChild(e.target);
     list.appendChild(newItem);
-});
-
-listItem.addEventListener("click", function () {
-    document.this.remove();
-});
+    textInput.value = "";
+}
